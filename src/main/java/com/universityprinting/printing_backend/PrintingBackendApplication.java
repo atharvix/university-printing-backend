9f45e8f8925682fd5bc6b56ccdbcb3dafae9e076
User <mehrotraatharv15@gmail.com>
2026-08-31 01:50:52 +0530

    initial commit

---
+++ src/main/java/com/universityprinting/printing_backend/PrintingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.universityprinting.printing_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrintingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrintingBackendApplication.class, args);
+	}
+
+}
