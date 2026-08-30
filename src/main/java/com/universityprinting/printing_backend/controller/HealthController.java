9dd7def0b8295adbd8cc87f046fdc1ef1477937a
User <mehrotraatharv15@gmail.com>
2026-08-31 02:04:08 +0530

    rest controller for health test commit

---
+++ src/main/java/com/universityprinting/printing_backend/controller/HealthController.java
@@ -0,0 +1,16 @@
+package com.universityprinting.printing_backend.controller;
+
+import java.util.Map;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HealthController {
+
+    @GetMapping("/health")
+    public Map<String, String> getHealth() {
+        return Map.of("status", "UP");
+    }
+}
